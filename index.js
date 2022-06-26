5621480aaf65bea96c59705951b01a65570f110c
mesmeri <angels_ua@ukr.net>
2022-06-26 14:48:32 +0300

    feat: create server and draw circle function

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-import Jimp from 'jimp';
-import {httpServer} from './src/http_server/index.js';
-import robot from 'robotjs';
-import { WebSocketServer } from 'ws';
+import Jimp from "jimp";
+import { httpServer } from "./src/http_server/index.js";
+import robot from "robotjs";
+import { WebSocketServer } from "ws";
+
+import drawCircle from "./src/draw_utils/draw_circle.js";
 
 const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (wss) => {
+  wss.on("message", (message) => {
+    const command = message.toString();
+
+    if (command.startsWith("draw_circle")) {
+      const radius = command.split(" ")[1];
+
+      drawCircle(radius);
+    }
+  });
+
+  wss.send("connection response");
+});
---
+++ src/draw_utils/draw_circle.js
@@ -0,0 +1,14 @@
+const drawCircle = (radius) => {
+  const mousePos = robot.getMousePos();
+
+  for (let i = 0; i <= Math.PI * 2; i += 0.01) {
+    const x = mousePos.x + radius * Math.cos(i);
+    const y = mousePos.y + radius * Math.sin(i);
+
+    robot.mouseToggle("down", "left");
+    robot.dragMouse(x, y);
+    robot.mouseToggle("up", "left");
+  }
+};
+
+export default drawCircle;
